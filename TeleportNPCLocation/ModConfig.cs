6619152003b0b80880b3b77d2e79a57f2d041177
ccr <chencrstu@163.com>
2023-06-30 10:05:51 +0800

    EDIT:change default toggle button

--- TeleportNPCLocation/ModConfig.cs
+++ TeleportNPCLocation/ModConfig.cs
@@ -7,7 +7,7 @@ namespace TeleportNPCLocation
     public class ModConfig
     {
         /// <summary>The keys which toggle npc menu.</summary>
-        public KeybindList ToggleNPCMenu { get; set; } = new(SButton.P);
+        public KeybindList ToggleNPCMenu { get; set; } = new(SButton.OemTilde);
 
         /// <summary>show more npc info.</summary>
         public bool showMoreInfo { get; set; } = true;
